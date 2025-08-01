61f4764781203f5094f524c0bed8073b0c618f43
Kimleng <kimleng-lim@outlook.com>
2025-08-01 17:23:21 -0400

    Add GenericList for reusability, type safe and performance issue.

---
+++ Generics.ConsoleApp/GenericList.cs
@@ -0,0 +1,15 @@
+public class GenericList<T>
+{
+    public void Add(T item)
+    {
+        throw new NotImplementedException("Add method is not implemented yet.");
+    }
+
+    public T this[int index]
+    {
+        get
+        {
+            throw new NotImplementedException("Indexer is not implemented yet.");
+        }
+    }
+}
--- Generics.ConsoleApp/Program.cs
+++ Generics.ConsoleApp/Program.cs
@@ -1,11 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 var book = new Book("1234567890", "C# Programming");
 
-var list = new ObjectList();
+var list = new GenericList<int>();
 list.Add(10);
 Console.WriteLine(list[0]); // Output: 10
 
-list.Add(book);
-var unboxBook = list[1] as Book;
-Console.WriteLine(unboxBook?.Title); // Output: C# Programming
-
+var bookList = new GenericList<Book>();
+bookList.Add(book);
+Console.WriteLine(bookList[0].Title); // Output: C# Programming
